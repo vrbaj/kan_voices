0f772a482ed5561a1ff041ff40186b1e312466af
vrbaj <vrbik.vrbik@gmail.com>
2024-06-04 15:09:43 +0900

    kan_arch_tester.py finished

--- kan_arch_tester.py
+++ kan_arch_tester.py
@@ -14,7 +14,7 @@ dataset["test_label"] = torch.from_numpy(np.array(test_set["labels"]))
 dataset["train_label"] = dataset["train_label"].type(torch.LongTensor)
 dataset["test_label"] = dataset["test_label"].type(torch.LongTensor)
 
-model = KAN(width=[5, 2], grid=3, k=3)
+model = KAN(width=[5, 5, 2], grid=3, k=3)
 
 
 def train_acc():
@@ -25,9 +25,9 @@ def test_acc():
     return torch.mean((torch.argmax(model(dataset['test_input']), dim=1) == dataset['test_label']).float())
 
 
-results = model.train(dataset, opt="LBFGS",
-                      steps=20,
+results = model.train(dataset, opt="Adam",
+                      steps=100, batch=-1,
                       metrics=(train_acc, test_acc),
                       loss_fn=torch.nn.CrossEntropyLoss())
 
-results['train_acc'][-1], results['test_acc'][-1]
+print(results['train_acc'][-1], results['test_acc'][-1])
