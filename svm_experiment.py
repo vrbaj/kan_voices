c1db4f61b3dd21640c99dfeb61018e6c490e8ddc
vrbaj <vrbik.vrbik@gmail.com>
2024-06-05 13:04:24 +0900

    results to file added

--- svm_experiment.py
+++ svm_experiment.py
@@ -1,13 +1,15 @@
 from sklearn.svm import SVC
 import librosa
 from sklearn.pipeline import make_pipeline
-from sklearn.metrics import confusion_matrix, classification_report, accuracy_score, precision_score, recall_score, matthews_corrcoef
+from sklearn.metrics import confusion_matrix, classification_report, accuracy_score, precision_score, recall_score, f1_score
 import matplotlib.pyplot as plt
 import seaborn as sns
 from tqdm import tqdm
+from tqdm import contrib
 import pickle
 import numpy as np
-from itertools import product
+import csv
+
 
 train_set = pickle.load(open("train_set.pk", "rb"))
 test_set = pickle.load(open("test_set.pk", "rb"))
@@ -21,29 +23,37 @@ best_acc = 0.
 best_report = None
 best_conf_matrix = None
 best_c = 0
-best_mcc = 0
+best_f1 = 0
+best_specificity = 0
 
-for c, cls_weight in tqdm(product(range(100, 10000, 10), range(10, 100, 10))):
+for c, cls_weight in tqdm(contrib.itertools.product(range(100, 10000, 10), range(10, 100, 10))):
         # clf = make_pipeline(SVC(gamma="auto", kernel="rbf", C=c))
         clf = SVC(gamma="auto", kernel="rbf", C=c, class_weight={0: cls_weight / 10})
         clf.fit(dataset["train_input"], dataset["train_label"])
 
         y_pred = clf.predict(dataset["test_input"])
         acc_score = accuracy_score(dataset["test_label"], y_pred)
-        f1 = matthews_corrcoef(dataset["test_label"], y_pred)
-        if f1 > best_mcc:
+        f1 = f1_score(dataset["test_label"], y_pred)
+        precision = precision_score(dataset["test_label"], y_pred)
+        recall = recall_score(dataset["test_label"], y_pred)
+        tn, fp, fn, tp = confusion_matrix(dataset["test_label"], y_pred).ravel()
+        specificity = tn / (tn + fp)
+        with open("results.csv", "a") as f:
+            csv_writer = csv.writer(f)
+            csv_writer.writerow([c, cls_weight ,acc_score, f1, precision, recall, specificity])
+        if specificity > best_specificity:
             best_precision = precision_score(dataset["test_label"], y_pred)
             best_recall = recall_score(dataset["test_label"], y_pred)
             best_acc = acc_score
             best_report = classification_report(dataset["test_label"], y_pred, digits=5)
             best_conf_matrix = confusion_matrix(dataset["test_label"], y_pred)
             best_c = c
-            best_mcc = f1
+            best_f1 = f1
             tn, fp, fn, tp = confusion_matrix(dataset["test_label"], y_pred).ravel()
-            specificity = tn / (tn + fp)
-print(f"Best c: {best_c:1.5f}, best mcc: {best_mcc}"
+            best_specificity = tn / (tn + fp)
+print(f"Best c: {best_c:1.5f}, best f1: {best_f1}"
       f" acc: {best_acc}, recall: {best_recall},"
-      f" specificity: {specificity}")
+      f" specificity: {best_specificity}")
 print(best_report)
 print(best_conf_matrix)
 # Plot confusion matrix
