357cd7f1ba6524c82c0c2d64473f5d7ef7db27cd
vrbaj <vrbik.vrbik@gmail.com>
2024-06-07 17:15:35 +0900

    modify_set_makers.py updated with new feature (0,1 for NaN)

--- modify_set_makers.py
+++ modify_set_makers.py
@@ -4,7 +4,9 @@ set_maker_folder = Path(".").joinpath("evaluated_datasets")
 dst_folder = Path(".")
 for idx, set_maker in enumerate(set_maker_folder.iterdir()):
     text = set_maker.joinpath("set_maker.py").read_text()
-    new_file = text.replace("not np.isnan(np.array(features)).any() and", "")
-    new_file = text.replace("discard_samples=discard_time)\n", "discard_samples=discard_time)\n        features = np.nan_to_num(np.array(features), copy=True, nan=0)\n")
+    # new_file = text.replace("not np.isnan(np.array(features)).any() and", "")
+    # new_file = text.replace("discard_samples=discard_time)\n", "discard_samples=discard_time)\n        features = np.nan_to_num(np.array(features), copy=True, nan=0)\n")
+    new_file = text.replace("features = np.nan_to_num(np.array(features), copy=True, nan=0)", "features.append(1 if np.isnan(features).any() else 0)\n        features = np.nan_to_num(np.array(features), copy=True, nan=0)\n")
     with open(dst_folder.joinpath(f"setmaker{idx}.py"), "w") as file:
         file.write(new_file)
+
