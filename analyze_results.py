cf8cd114068f4fc4d6419f9027010be27e0f9e8b
vrbaj <vrbik.vrbik@gmail.com>
2024-06-07 11:07:14 +0900

    analyze_results.py updated

--- analyze_results.py
+++ analyze_results.py
@@ -1,7 +1,8 @@
 import csv
+from pathlib import Path
 
 # File path for the CSV file
-file_path = 'results_var17+.csv'
+results_folder_path = Path(".").joinpath("results")
 
 # Function to iterate over the CSV file
 def iterate_csv(file_path):
@@ -11,10 +12,11 @@ def iterate_csv(file_path):
             # Iterate over the rows in the CSV file
             for idx, row in enumerate(csv_reader):
                 if len(row) > 1 and idx > 1:
-                    if float(row[-1]) > 0.90 and float(row[-2]) > 0.91:
+                    if float(row[-1]) > 0.83 and float(row[1]) > 0.85:
                         print(row)
     except Exception as e:
         print(f"An error occurred: {e}")
 
 # Call the function
-iterate_csv(file_path)
+for results_folder in results_folder_path.iterdir():
+    iterate_csv(results_folder.joinpath("results.csv").resolve())
