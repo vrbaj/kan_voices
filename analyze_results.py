1cab64479d3f5de64caaa37b7a565129a9b3ace6
steinbaj <steinbach.jakub@gmail.com>
2024-06-12 07:25:28 +0200

    Changed to print data from the new result tables.

--- analyze_results.py
+++ analyze_results.py
@@ -1,22 +1,31 @@
-import csv
 from pathlib import Path
+import pandas as pd
+import json
 
-# File path for the CSV file
-results_folder_path = Path(".").joinpath("results")
+min_acc = 0.8  # 0.8577
+min_recall = 0.83  # 0.8759
+min_spec = 0.83  # 0.8394
+results_dir = Path("results")
+temp_df_list = []
+for path in results_dir.glob("**/results.csv"):
+    temp_df_list.append(pd.read_csv(path))
+    temp_df_list[-1]["data"] = path.parent.name
 
-# Function to iterate over the CSV file
-def iterate_csv(file_path):
-    try:
-        with open(file_path, mode='r', newline='') as csvfile:
-            csv_reader = csv.reader(csvfile)
-            # Iterate over the rows in the CSV file
-            for idx, row in enumerate(csv_reader):
-                if len(row) > 1 and idx > 1:
-                    if float(row[-1]) > 0.83 and float(row[1]) > 0.85:
-                        print(row)
-    except Exception as e:
-        print(f"An error occurred: {e}")
+table_results = pd.concat(temp_df_list)
+table_results = table_results[(table_results.mean_test_accuracy >= min_acc) &
+                              (table_results.mean_test_recall >= min_recall) &
+                              (table_results.mean_test_specificity >= min_spec)].sort_values("mean_test_accuracy")
 
-# Call the function
-for results_folder in results_folder_path.iterdir():
-    iterate_csv(results_folder.joinpath("results.csv").resolve())
+for _, row in table_results.iterrows():
+    str_params = ""
+    for key, val in json.loads(row["params"].replace("'", '"')).items():
+        if key == "classifier__C":
+            str_params += f"{key.split('__')[1]}: {val:>5} - "
+        elif key == "classifier__kernel":
+            str_params += f"{key.split('__')[1]}: {val:>4} - "
+        else:
+            str_params += f"{key.split('__')[1]}: {val} - "
+
+    print(
+        f'{row["data"]} | {str_params[:-2]}| Acc: {row["mean_test_accuracy"]:.5f} - Sen: {row["mean_test_recall"]:.5f}'
+        f' - Spe: {row["mean_test_specificity"]:.5f}')
