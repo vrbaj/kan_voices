0a3aaedd514d5bcecb3dda05f3f04fbe84c6a0ba
vrbaj <vrbik.vrbik@gmail.com>
2024-06-12 13:32:34 +0900

    analyze_results.py tweaked

--- analyze_results.py
+++ analyze_results.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 # File path for the CSV file
 results_folder_path = Path(".").joinpath("results")
-
+best_makers = []
 # Function to iterate over the CSV file
 def iterate_csv(file_path):
     try:
@@ -12,11 +12,12 @@ def iterate_csv(file_path):
             # Iterate over the rows in the CSV file
             for idx, row in enumerate(csv_reader):
                 if len(row) > 1 and idx > 1:
-                    if float(row[-1]) > 0.83 and float(row[1]) > 0.85:
-                        print(row)
+                    if float(row[-1]) > 0.85 and float(row[1]) > 0.8577:
+                        best_makers.append(row[0])
     except Exception as e:
         print(f"An error occurred: {e}")
 
 # Call the function
 for results_folder in results_folder_path.iterdir():
     iterate_csv(results_folder.joinpath("results.csv").resolve())
+print(set(best_makers))
