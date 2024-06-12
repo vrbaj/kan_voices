e60d48dee15c4a706f360b4a3f5334cf22aa46dc
steinbaj <steinbach.jakub@gmail.com>
2024-06-12 07:31:02 +0200

    Changed to print data from the new result tables.

--- analyze_results.py
+++ analyze_results.py
@@ -2,9 +2,10 @@ from pathlib import Path
 import pandas as pd
 import json
 
-min_acc = 0.8  # 0.8577
-min_recall = 0.83  # 0.8759
-min_spec = 0.83  # 0.8394
+
+min_acc = 0.85  # 0.8577
+min_recall = 0.85  # 0.8759
+min_spec = 0.85  # 0.8394
 results_dir = Path("results")
 temp_df_list = []
 for path in results_dir.glob("**/results.csv"):
